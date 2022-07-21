7abd0ad5580fc8f6a8d72482896bb5f4e2a09f43
kotamadeo <kotamadeo@gmail.com>
2022-07-21 14:09:58 +0300

    Minor self-security fixes.

--- src/main/java/com/gmail/at/kotamadeo/config/ServletDispatcherConfig.java
+++ src/main/java/com/gmail/at/kotamadeo/config/ServletDispatcherConfig.java
@@ -1,6 +1,5 @@
 package com.gmail.at.kotamadeo.config;
 
-import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
--- src/main/java/com/gmail/at/kotamadeo/config/SpringConfig.java
+++ src/main/java/com/gmail/at/kotamadeo/config/SpringConfig.java
@@ -47,7 +47,6 @@ public class SpringConfig implements WebMvcConfigurer {
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
-        resolver.setTemplateEngine(templateEngine());
         resolver.setCharacterEncoding("UTF-8");
         resolver.setContentType("text/html; charset=UTF-8");
         registry.viewResolver(resolver);
